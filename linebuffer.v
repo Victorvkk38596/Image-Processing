8ade17eb36609e5debf4e3d0eecf18af84234553
Victorvkk38596 <88576590+Victorvkk38596@users.noreply.github.com>
2025-07-16 16:07:48 +0530

    Update linebuffer.v

--- linebuffer.v
+++ linebuffer.v
@@ -1,3 +1,5 @@
+//Single line buffer, will be instantiating 4 line buffers in top module
+
 module lineBuffer(
 input   i_clk,
 input   i_rst,
@@ -7,16 +9,20 @@ output [23:0] o_data,
 input i_rd_data
 );
 
-reg [7:0] line [511:0];
+reg [7:0] line [511:0];     //Stores a row of the 512x512 image.
 reg [8:0] wrPntr;
 reg [8:0] rdPntr;
 
+
+//Writing data into the buffer
 always @(posedge i_clk)
 begin
     if(i_data_valid)
         line[wrPntr] <= i_data;
 end
 
+
+//Moving the pointer forward
 always @(posedge i_clk)
 begin
     if(i_rst)
@@ -25,8 +31,11 @@ begin
         wrPntr <= wrPntr + 1'd1;
 end
 
+//Outputs 3 contiguous image pixel in a line for the 3x3 kernel
 assign o_data = {line[rdPntr],line[rdPntr+1],line[rdPntr+2]};
 
+
+//Read pointer control, similar to write pointer. Only moved by a single pixel, assuming a stride size of 1.
 always @(posedge i_clk)
 begin
     if(i_rst)
